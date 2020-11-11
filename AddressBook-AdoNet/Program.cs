e6090d12f2979f45362adbb6fd0053555e5f6805
kretika arora <kretikaarora@gmail.com>
2020-11-11 18:49:21 +0530

    [Kretika Arora]Finding Contacts in a date range

--- AddressBook-AdoNet/Program.cs
+++ AddressBook-AdoNet/Program.cs
@@ -9,19 +9,17 @@ using System;
 
 namespace AddressBook_AdoNet
 {
+    /// <summary>
+    /// Program Class
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Address Book AdoNet");
-            AddressBookOperations addressBookOperations = new AddressBookOperations();
-            ContactDetails contactDetails =  new ContactDetails();
-            contactDetails.firstName = "daisy";
-            contactDetails.lastName = "erison";
-            contactDetails.phoneNumber = 8111100011;
-            contactDetails.email = "eric@skype.com";
-            contactDetails.addressBookName = "First";
-            Console.WriteLine("bool value"+addressBookOperations.UpdateAddressBookDetails(contactDetails)); 
+            AddressBookOperations addressBookOperations = new AddressBookOperations();           
+            Console.WriteLine(addressBookOperations.GetContactDetailsBetweenDateRange());
+            
         }
     }
 }
